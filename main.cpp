de6776c06c8a6e769cbc4d495f605b11e4a7b017
Darren Bowers <darren.bowers@g.austincc.edu>
2022-02-09 15:06:32 -0600

    made a loop to randomly generate a 0 or 1 and push or pop depending on result

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ Purpose: Stacks introduction assignment
 #include "main.h"
 
 int main(int argc, char** argv){
+    srand(time(NULL));
     myStack myStack1(SIZE);
 
     if(myStack1.isEmpty()){
@@ -26,7 +27,6 @@ int main(int argc, char** argv){
         }else{
             cout << "Overflow Error" << endl;
         }
-        
     }
 
     cout << endl;
@@ -56,6 +56,60 @@ int main(int argc, char** argv){
     } else{
         cout << "Stack is not empty" << endl;
     }
+
+    cout << endl;
+
+    for(int i=0; i<=SIZE+10; i++){
+        if(myStack1.push(i)){
+            cout << "Element Insterted" << endl;
+        }else{
+            cout << "Overflow Error" << endl;
+        }
+    }
+
+    cout << endl;
+
+    for(int i=0; i<=SIZE+10; i++){
+        try{
+            cout << myStack1.pop() << endl;
+            cout << myStack1.peek() << endl;
+        } catch(...){
+            cout << "Underflow Error" << endl;
+        }
+    }
+
+    cout << endl;
+
+    if(myStack1.isEmpty()){
+        cout << "Stack is empty" << endl;
+    } else{
+        cout << "Stack is not empty" << endl;
+    }
+
+    cout << endl;
+
+    for(int i=0; i<=SIZE+100; i++){
+        int random = rand() % 2;
+        cout << "random is " << random << endl;
+        if(random == 0){
+            if(myStack1.push(i)){
+            cout << "Element Insterted" << endl;
+            }else{
+            cout << "Overflow Error" << endl;
+            }
+        }else{
+            try{
+                cout << myStack1.pop() << endl;
+            }catch(...){
+                cout << "Underflow Error" << endl;
+            }
+        }
+    }
+
+    cout << endl;
+
+    myStack1.readStack();
+
 
     return 0;
 }

8643565c6dee3e283656c8249ded51236c91e5af
Darren Bowers <darren.bowers@g.austincc.edu>
2022-02-09 13:13:06 -0600

    prototyped and stubbed expected methods

--- stack.cpp
+++ stack.cpp
@@ -4,6 +4,29 @@ Coding 03
 Purpose: Stacks introduction assignment
 **********************/
 
-
-
 #include "stack.h"
+
+myStack::myStack(int size){
+    top = -1;
+    if(size > DEFAULTSIZE){
+        stack = new int[size];
+    }else{
+        stack = new int[DEFAULTSIZE];
+    }
+}
+
+myStack::~myStack(){
+    delete[] stack;
+}
+
+bool myStack::push(int number){
+    return true;
+}
+
+bool myStack::pop(){
+    return true;
+}
+
+bool myStack::isEmpty(){
+    return true;
+}
--- stack.h
+++ stack.h
@@ -9,6 +9,22 @@ Purpose: Stacks introduction assignment
 #ifndef STACK_STACK_H
 #define STACK_STACK_H
 
+#define DEFAULTSIZE 1
+
+class myStack {
+
+    public:
+        myStack(int);
+        ~myStack();
+        bool push(int);
+        bool pop();
+        bool isEmpty();
+
+    private:
+        int top;
+        int *stack;
+
+};
 
 
 

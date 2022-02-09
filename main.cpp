45bb533776df0e3dbd10323a323904b7a5db2279
Darren Bowers <darren.bowers@g.austincc.edu>
2022-02-09 13:48:58 -0600

    added a loop to push well past top of stack and print a message dependant on success, called readStack after to test if elements were pushed correctly

--- main.cpp
+++ main.cpp
@@ -10,7 +10,19 @@ Purpose: Stacks introduction assignment
 #include "main.h"
 
 int main(int argc, char** argv){
-    cout << "Hello, World!" << endl;
+    myStack myStack1(SIZE);
+
+    for(int i=0; i<=SIZE+10; i++){
+        if(myStack1.push(i)){
+            cout << "Element Insterted" << endl;
+        }else{
+            cout << "Stack Full" << endl;
+        }
+        
+    }
+
+    cout << endl;
+    myStack1.readStack();
 
     return 0;
 }

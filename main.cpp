1658f21e5ae369dc8e20beeb406fec2f5a946481
Darren Bowers <darren.bowers@g.austincc.edu>
2022-02-09 14:32:27 -0600

    added a few calls for isEmpty and a loop to test pop. Also updated error for push

--- main.cpp
+++ main.cpp
@@ -12,17 +12,50 @@ Purpose: Stacks introduction assignment
 int main(int argc, char** argv){
     myStack myStack1(SIZE);
 
+    if(myStack1.isEmpty()){
+        cout << "Stack is empty" << endl;
+    } else{
+        cout << "Stack is not empty" << endl;
+    }
+
+    cout << endl;
+
     for(int i=0; i<=SIZE+10; i++){
         if(myStack1.push(i)){
             cout << "Element Insterted" << endl;
         }else{
-            cout << "Stack Full" << endl;
+            cout << "Overflow Error" << endl;
         }
         
     }
 
     cout << endl;
     myStack1.readStack();
+    cout << endl;
+
+    if(myStack1.isEmpty()){
+        cout << "Stack is empty" << endl;
+    } else{
+        cout << "Stack is not empty" << endl;
+    }
+
+    cout << endl;
+
+    for(int i=0; i<=SIZE+10; i++){
+        try{
+            cout << myStack1.pop() << endl;
+        } catch(...){
+            cout << "Underflow Error" << endl;
+        }
+    }
+
+    cout << endl;
+
+    if(myStack1.isEmpty()){
+        cout << "Stack is empty" << endl;
+    } else{
+        cout << "Stack is not empty" << endl;
+    }
 
     return 0;
 }
